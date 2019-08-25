fca5e6c283d1d258fb3a5b57fbe49fb8c334dfbe
翁于宸 <open852134@gmail.com>
2019-08-26 01:11:55 +0800

    add video

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const http = require('http');
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = process.env.PORT || 1337;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! XXXXXXXXXXXXXX");
-
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+app.get("/video", function(req, res) {
+  res.writeHead(200, { "Content-Type": "video/avi" });
+  fs.createReadStream("test.mp4")
+    .pipe(res)
+    .on("finish", () => {
+      console.log("finish");
+    });
+  // res.send("Hello World!");
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, function() {
+  console.log("Example app listening on port 3000!");
+});
